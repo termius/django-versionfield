277e350e42a075a7373eaf6b787482cac3186fe6
Antoine Nguyen <tonio@ngyn.org>
2014-12-20 14:50:18 +0100

    PEP8 fixes

--- versionfield/forms.py
+++ versionfield/forms.py
@@ -1,21 +1,31 @@
+"""Custom form fields."""
+
 from django import forms
-from version import Version
-from constants import DEFAULT_NUMBER_BITS
-from utils import convert_version_string_to_int
+
+from .version import Version
+from .constants import DEFAULT_NUMBER_BITS
+from .utils import convert_version_int_to_string
+
 
 class VersionField(forms.IntegerField):
-	def __init__(self,number_bits=DEFAULT_NUMBER_BITS,**kwargs):
-		self.number_bits = number_bits
-		return super(VersionField, self).__init__(**kwargs)
 
-	def to_python(self,value):
-		"""
-		Verifies that value can be converted to a Version object
-		"""
-		if not value:
-			return None
+    """A form field dedicated to version numbers."""
 
-		if isinstance(value,basestring):
-			return Version(value,self.number_bits)		
+    def __init__(self, number_bits=DEFAULT_NUMBER_BITS, **kwargs):
+        self.number_bits = number_bits
+        return super(VersionField, self).__init__(**kwargs)
 
-		return Version(convert_version_int_to_string(value,self.number_bits),self.number_bits)
+    def to_python(self, value):
+        """
+        Verifies that value can be converted to a Version object
+        """
+        if not value:
+            return None
+
+        if isinstance(value, basestring):
+            return Version(value, self.number_bits)
+
+        return Version(
+            convert_version_int_to_string(value, self.number_bits),
+            self.number_bits
+        )
--- versionfield/tests/__init__.py
+++ versionfield/tests/__init__.py
@@ -2,108 +2,114 @@ import unittest
 
 from django.test import TestCase
 from django.db import models
+
 from .. import VersionField
-from versionfield.constants import DEFAULT_NUMBER_BITS
-from versionfield.version import Version
+from ..constants import DEFAULT_NUMBER_BITS
+from ..version import Version
+
 
 class DummyModel(models.Model):
-	version = VersionField()
+    version = VersionField()
+
 
 class VersionFieldTest(TestCase):
-	def setUp(self):
-		DummyModel.objects.create(version="0.1")
-		DummyModel.objects.create(version="1.0")
-		DummyModel.objects.create(version="1.0.1")
 
-	def test_get_by_exact_version(self):
-		thing = DummyModel.objects.get(version="0.1")
-		self.assertEqual(thing.version,"0.1")
-		self.assertEqual(thing.version,"0.1.0")
+    def setUp(self):
+        DummyModel.objects.create(version="0.1")
+        DummyModel.objects.create(version="1.0")
+        DummyModel.objects.create(version="1.0.1")
 
-	def test_filter_by_greater_than_version(self):
-		things = DummyModel.objects.filter(version__gt="0.1")
-		self.assertEqual(len(things),2)
+    def test_get_by_exact_version(self):
+        thing = DummyModel.objects.get(version="0.1")
+        self.assertEqual(thing.version,"0.1")
+        self.assertEqual(thing.version,"0.1.0")
 
-		things = DummyModel.objects.filter(version__gt="1.0")
-		self.assertEqual(len(things),1)
+    def test_filter_by_greater_than_version(self):
+        things = DummyModel.objects.filter(version__gt="0.1")
+        self.assertEqual(len(things),2)
 
-		things = DummyModel.objects.filter(version__gt="1.0.1")
-		self.assertEqual(len(things),0)
+        things = DummyModel.objects.filter(version__gt="1.0")
+        self.assertEqual(len(things),1)
 
-	def test_filter_by_less_than_version(self):
-		things = DummyModel.objects.filter(version__lt="0.1")
-		self.assertEqual(len(things),0)
+        things = DummyModel.objects.filter(version__gt="1.0.1")
+        self.assertEqual(len(things),0)
 
-		things = DummyModel.objects.filter(version__lt="1.0")
-		self.assertEqual(len(things),1)
+    def test_filter_by_less_than_version(self):
+        things = DummyModel.objects.filter(version__lt="0.1")
+        self.assertEqual(len(things),0)
 
-		things = DummyModel.objects.filter(version__lt="1.0.1")
-		self.assertEqual(len(things),2)
+        things = DummyModel.objects.filter(version__lt="1.0")
+        self.assertEqual(len(things),1)
 
-	def test_overflow_number(self):
-		error_occured = False
-		try:
-			overflow = DummyModel.objects.create(version="1.999.1")
-		except ValueError:
-			error_occured = True
-		self.assertTrue(error_occured)
+        things = DummyModel.objects.filter(version__lt="1.0.1")
+        self.assertEqual(len(things),2)
+
+    def test_overflow_number(self):
+        error_occured = False
+        try:
+            overflow = DummyModel.objects.create(version="1.999.1")
+        except ValueError:
+            error_occured = True
+        self.assertTrue(error_occured)
+
 
 class DummyModelCustomBit(models.Model):
-	version = VersionField(number_bits=(8,16,8))
+    version = VersionField(number_bits=(8, 16, 8))
+
 
 class VersionFieldCustomBitsTest(TestCase):
-	def setUp(self):
-		DummyModelCustomBit.objects.create(version="1.999.1")
+    def setUp(self):
+        DummyModelCustomBit.objects.create(version="1.999.1")
 
-	def test_get_by_exact_version(self):
-		thing = DummyModelCustomBit.objects.get(version="1.999.1")
-		self.assertEqual(thing.version,"1.999.1")
+    def test_get_by_exact_version(self):
+        thing = DummyModelCustomBit.objects.get(version="1.999.1")
+        self.assertEqual(thing.version, "1.999.1")
 
 
 class VersionObjectTestCase(unittest.TestCase):
 
-        def test_equal_operator(self):
-                self.assertEqual(
-                        Version("1.2.3", DEFAULT_NUMBER_BITS),
-                        Version("1.2.3", DEFAULT_NUMBER_BITS),
-                )
+    def test_equal_operator(self):
+        self.assertEqual(
+            Version("1.2.3", DEFAULT_NUMBER_BITS),
+            Version("1.2.3", DEFAULT_NUMBER_BITS),
+        )
 
-        def test_lt_operator(self):
-                self.assertTrue(
-                        Version("1.2.3", DEFAULT_NUMBER_BITS) <
-                        Version("1.2.4", DEFAULT_NUMBER_BITS)
-                )
-                self.assertFalse(
-                        Version("1.2.4", DEFAULT_NUMBER_BITS) <
-                        Version("1.2.3", DEFAULT_NUMBER_BITS)
-                )
+    def test_lt_operator(self):
+        self.assertTrue(
+            Version("1.2.3", DEFAULT_NUMBER_BITS) <
+            Version("1.2.4", DEFAULT_NUMBER_BITS)
+        )
+        self.assertFalse(
+            Version("1.2.4", DEFAULT_NUMBER_BITS) <
+            Version("1.2.3", DEFAULT_NUMBER_BITS)
+        )
 
-        def test_le_operator(self):
-                self.assertTrue(
-                        Version("1.2.3", DEFAULT_NUMBER_BITS) <=
-                        Version("1.2.4", DEFAULT_NUMBER_BITS)
-                )
-                self.assertTrue(
-                        Version("1.2.4", DEFAULT_NUMBER_BITS) <=
-                        Version("1.2.4", DEFAULT_NUMBER_BITS)
-                )
+    def test_le_operator(self):
+        self.assertTrue(
+            Version("1.2.3", DEFAULT_NUMBER_BITS) <=
+            Version("1.2.4", DEFAULT_NUMBER_BITS)
+        )
+        self.assertTrue(
+            Version("1.2.4", DEFAULT_NUMBER_BITS) <=
+            Version("1.2.4", DEFAULT_NUMBER_BITS)
+        )
 
-        def test_gt_operator(self):
-                self.assertTrue(
-                        Version("1.2.4", DEFAULT_NUMBER_BITS) >
-                        Version("1.2.3", DEFAULT_NUMBER_BITS)
-                )
-                self.assertFalse(
-                        Version("2.3.4", DEFAULT_NUMBER_BITS) >
-                        Version("3.2.1", DEFAULT_NUMBER_BITS)
-                )
+    def test_gt_operator(self):
+        self.assertTrue(
+            Version("1.2.4", DEFAULT_NUMBER_BITS) >
+            Version("1.2.3", DEFAULT_NUMBER_BITS)
+        )
+        self.assertFalse(
+            Version("2.3.4", DEFAULT_NUMBER_BITS) >
+            Version("3.2.1", DEFAULT_NUMBER_BITS)
+        )
 
-        def test_ge_operator(self):
-                self.assertTrue(
-                        Version("1.2.4", DEFAULT_NUMBER_BITS) >=
-                        Version("1.2.3", DEFAULT_NUMBER_BITS)
-                )
-                self.assertTrue(
-                        Version("3.3.3", DEFAULT_NUMBER_BITS) <=
-                        Version("3.3.3", DEFAULT_NUMBER_BITS)
-                )
+    def test_ge_operator(self):
+        self.assertTrue(
+            Version("1.2.4", DEFAULT_NUMBER_BITS) >=
+            Version("1.2.3", DEFAULT_NUMBER_BITS)
+        )
+        self.assertTrue(
+            Version("3.3.3", DEFAULT_NUMBER_BITS) <=
+            Version("3.3.3", DEFAULT_NUMBER_BITS)
+        )
