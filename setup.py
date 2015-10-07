6238e0a96b6ed5f2823d990461e2951fc1a06898
Antoine Nguyen <tonio@ngyn.org>
2015-10-07 09:29:53 +0200

    Release 0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="django-versionfield2",
-    version="0.3.3",
+    version="0.4",
     url='https://github.com/tonioo/django-versionfield',
     license='BSD',
     description="A DB Independent Custom Django Field for storing Version numbers for fast indexing",
